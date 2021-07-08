c46a0ccbd6f50e0a2d827f9609c07962c3fc5cb4
Damien DURO <damien@weweb.io>
2021-07-08 10:52:47 +0200

    Version 3.0.0

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,46 +1,38 @@
 <template>
-  <div class="ww-date">
-    <wwObject v-bind="content.el" :wwProps="{ text: formatedDate }"></wwObject>
-  </div>
+    <div class="ww-date">
+        <wwElement v-bind="content.el" :ww-props="{ text: formatedDate }" />
+    </div>
 </template>
 
 <script>
-import * as dayjs from "dayjs";
-var customParseFormat = require("dayjs/plugin/customParseFormat");
+import * as dayjs from 'dayjs';
+const customParseFormat = require('dayjs/plugin/customParseFormat');
 
 export default {
-  wwDefaultContent: {
-    el: wwLib.element("ww-text"),
-    date: "2021-03-11T10:11:20.000+00:00",
-    format: "DD/MM/YYYY",
-    customFormat: "",
-  },
-  props: {
-    content: { type: "Object", required: true },
-  },
-  computed: {
-    formatedDate() {
-      return dayjs(this.content.date).format(this.formatToUse);
+    props: {
+        content: { type: Object, required: true },
     },
-    formatToUse() {
-      return this.content.customFormat.length
-        ? this.content.customFormat
-        : this.content.format;
+    wwDefaultContent: {
+        el: wwLib.element('ww-text'),
+        date: '2021-03-11T10:11:20.000+00:00',
+        format: 'DD/MM/YYYY',
+        customFormat: '',
     },
-  },
-  methods: {
-    goToDoc() {
-      console.log("https://day.js.org/docs/en/display/format#docsNav");
-      window.open(
-        "https://day.js.org/docs/en/display/format#docsNav",
-        "_blank"
-      );
+    computed: {
+        formatedDate() {
+            return dayjs(this.content.date).format(this.formatToUse);
+        },
+        formatToUse() {
+            return this.content.customFormat.length ? this.content.customFormat : this.content.format;
+        },
     },
-  },
-  mounted() {
-    dayjs.extend(customParseFormat);
-  },
+    mounted() {
+        dayjs.extend(customParseFormat);
+    },
+    methods: {
+        goToDoc() {
+            window.open('https://day.js.org/docs/en/display/format#docsNav', '_blank');
+        },
+    },
 };
 </script>
-
-<style lang="scss" scoped></style>

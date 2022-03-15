b22ba53207a976883702ca35e6dcbb92dbb6dfea
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2022-03-15 10:12:41 +0100

    update element: icon & placeholder

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,6 +1,12 @@
 <template>
-    <div class="ww-date">
+    <div class="ww-date" :class="{ editing: isEditing, selected: isSelected }">
         <wwElement v-bind="content.el" :ww-props="{ text: formatedDate }" />
+
+        <!-- wwEditor:start -->
+        <div class="ww-date__menu">
+            <wwEditorIcon small name="fontawesome/solid/calendar-day" />
+        </div>
+        <!-- wwEditor:end -->
     </div>
 </template>
 
@@ -11,8 +17,25 @@ const customParseFormat = require('dayjs/plugin/customParseFormat');
 export default {
     props: {
         content: { type: Object, required: true },
+        /* wwEditor:start */
+        wwEditorState: { type: Object, required: true },
+        /* wwEditor:end */
     },
     computed: {
+        isEditing() {
+            /* wwEditor:start */
+            return this.wwEditorState.editMode === wwLib.wwEditorHelper.EDIT_MODES.EDITION;
+            /* wwEditor:end */
+            // eslint-disable-next-line no-unreachable
+            return false;
+        },
+        isSelected() {
+            /* wwEditor:start */
+            return this.wwEditorState.isSelected;
+            /* wwEditor:end */
+            // eslint-disable-next-line no-unreachable
+            return false;
+        },
         formatedDate() {
             return dayjs(this.content.date).format(this.formatToUse);
         },
@@ -30,3 +53,57 @@ export default {
     },
 };
 </script>
+
+<style lang="scss" scoped>
+.ww-date {
+    /* wwEditor:start */
+    &.selected {
+        > .ww-date__menu {
+            opacity: 1;
+            pointer-events: all;
+        }
+    }
+    &.editing:hover {
+        & > .border {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            border: 1px solid var(--ww-editor-color);
+            pointer-events: none;
+            z-index: 10;
+        }
+        > .ww-date__menu {
+            opacity: 1;
+            pointer-events: all;
+        }
+    }
+    &__menu {
+        display: flex;
+        position: absolute;
+        top: 0px;
+        left: 5px;
+        transform: translate(-50%, -50%);
+        border-radius: 100%;
+        padding: var(--ww-spacing-01);
+        transition: opacity 0.2s ease;
+        z-index: 101;
+        cursor: pointer;
+        background-color: var(--ww-color-blue-500);
+        color: var(--ww-color-white);
+        justify-content: center;
+        align-items: center;
+        opacity: 0;
+        pointer-events: none;
+        &:after {
+            content: '';
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%) rotate(45deg);
+        }
+    }
+    /* wwEditor:end */
+}
+</style>
